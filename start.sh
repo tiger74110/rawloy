de34d397af0291b22e9ce244d7c6d2eaa50b5e94
tiger74110 <98722640+tiger74110@users.noreply.github.com>
2022-09-03 06:42:41 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,10 +3,10 @@
 # configs
 AUUID=a0b3ef90-3e02-01ec-6d3d-7242ac1300c3
 CADDYIndexPage=https://github.com/AYJCSGM/mikutap/archive/master.zip
-CONFIGCADDY=https://raw.githubusercontent.com/tiger74110/hctxay/master/etc/Caddyfile
-CONFIGXRAY=https://raw.githubusercontent.com/tiger74110/hctxay/master/etc/xray.json
+CONFIGCADDY=https://raw.githubusercontent.com/tiger74110/rawloy/master/etc/Caddyfile
+CONFIGXRAY=https://raw.githubusercontent.com/tiger74110/rawloy/master/etc/xray.json
 ParameterSSENCYPT=chacha20-ietf-poly1305
-StoreFiles=https://raw.githubusercontent.com/tiger74110/hctxay/master/etc/StoreFiles
+StoreFiles=https://raw.githubusercontent.com/tiger74110/rawloy/master/etc/StoreFiles
 #PORT=4433
 mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
